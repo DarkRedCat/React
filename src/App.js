95ecdd443c307f4bf8841076dceea5d9ffb60431
kabbyy2 <kabbyy2@gmail.com>
2022-02-15 23:01:12 +0200

    Restoration_of_thesystem_after_the_loss_of_several_Commits_2/2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import Header from "./components/Header/Header"
+import HeaderContainer from "./components/Header/HeaderContainer"
 import Nav from "./components/Nav/Nav"
 
 /*pages*/
 import ProfileContainer from "./components/Page_body/Profile/ProfileContainer"
 import Messages from "./components/Page_body/Block_Messages/Block_Messages"
-// import Music from "./components/Page_body/Music/Music"
-// import News from "./components/Page_body/News/News"
+
 import FindUsersContainer from "./components/Page_body/FindUsers/FindUsersContainer"
-// import Settings from "./components/Page_body/Settings/Settings"
+
 
 
 import './App.css';
@@ -18,16 +17,14 @@ import {Routes, Route} from "react-router-dom";
 const App = () => {
   return (
     <div className="app-wrapper">
-      <Header/>
+      <HeaderContainer/>
       <Nav />
       <main className="pageBody__content">
         <Routes>
           <Route path={"/profile/*",'/*'} element={<ProfileContainer />}/>
-          <Route path="/messages/*" element={ <Messages  />}/>
-          {/* <Route path="/music" element={< Music />}/>
-          <Route path="/news" element={< News />}/>*/}
-          <Route path="/find_users/*" element={ <FindUsersContainer  />}/>
-          {/* <Route path="/settings" element={< Settings />}/> */}
+          <Route path="/messages/*" element={ <Messages/>}/>
+         
+          <Route path="/find_users/*" element={ <FindUsersContainer/>}/>
         </Routes>
       </main>
     </div>
--- src/redux/reducer/FindUsers_reducer.js
+++ src/redux/reducer/FindUsers_reducer.js
@@ -17,41 +17,31 @@ let initialState = {
 
 const FindUsers_reducer = (state = initialState, action) => {
     switch(action.type){
-        case FOLLOW : {
-            return {
-                ...state,
-                Users : state.Users.map(u => {
-                   if( u.id === action.id){
-                    return {...u , followed : true}
-                   }
-                   return u
-                }),
-            }
+        case FOLLOW : {return {
+            ...state,Users : state.Users.map(u => {
+                if( u.id === action.id){return {...u , followed : true}}
+                return u
+            }),
         }
-        
-
-        case UN_FOLLOW : {
-            return{
-                ...state,
-                Users : state.Users.map(u => {
-                   if( u.id === action.id){
-                    return {...u , followed : false}
-                   }
-                   return u
-                }),
-            }
-        }
-        case SET_USERS : {return{...state,Users: action.users}}
-        case SET_CURRENT_PAGE : {return{...state, currentPage: action.currentPage}}
-        case SET_TOTAL_USERS_COUNT : {return{...state, totalUsersCount: action.totalUsersCount}}
-        case TOGGLE_IS_FETCHING : {return{...state, isFetching: action.isFetching}}
-        default: 
-            
-            return state
     }
-   
-}
 
+    case UN_FOLLOW : {
+        return{
+            ...state,
+             Users : state.Users.map(u => {
+                 if( u.id === action.id){return {...u , followed : false}}
+                return u
+            }),
+        }
+    }
+
+    case SET_USERS : {return{...state,Users: action.users}}
+    case SET_CURRENT_PAGE : {return{...state, currentPage: action.currentPage}}
+    case SET_TOTAL_USERS_COUNT : {return{...state, totalUsersCount: action.totalUsersCount}}
+    case TOGGLE_IS_FETCHING : {return{...state, isFetching: action.isFetching}}
+    default: return state
+    } 
+}
 export const follow = (id) =>({type: FOLLOW,id})
 export const unFollow = (id) =>({type:UN_FOLLOW ,id})
 export const setUsers = (users) =>({type:SET_USERS , users})
